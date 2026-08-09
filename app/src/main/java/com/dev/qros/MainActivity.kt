7997243d6502d3281ce5ee0a5cd4976bc151cacd
sv709 <sv709@yahoo.com>
2026-08-09 15:33:24 -0700

    update main activity to remove previews temporarily

--- app/src/main/java/com/dev/qros/MainActivity.kt
+++ app/src/main/java/com/dev/qros/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.dev.qros.ui.screens.cameraPermission.CameraPermissionGateway
 import com.dev.qros.ui.screens.QrosMainScreen
+import com.dev.qros.ui.screens.cameraPermission.CameraPermissionGateway
 import com.dev.qros.ui.theme.QrosTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,22 +23,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QrosTheme {
-        Greeting("Android")
     }
 }
